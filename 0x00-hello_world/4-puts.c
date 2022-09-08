168f4690003e836fd93523ca652b6f658ec364cb
hilm <you@example.com>
2022-09-08 13:41:39 -0700

    outPUT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #include<stdio.h>
-int main(){char Mystr[] = ""Programming is like building a multilingual puzzle \n"; puts(Mystr); return 0;}
+
+int mainv(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle \n";
+	puts(Mystr); return 0;
+}
