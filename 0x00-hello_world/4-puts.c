8cdebfb83d7c1f8e9cb9352cf51f8d0123f35743
hilm <you@example.com>
2022-09-08 14:21:06 -0700

    outPUT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-/*
+/**
  *this is the first c program
  */
 int main(void)
-/*the entry point
+/**the entry point
  *
  */
 {
 
-	char String[] ="\"Programming is like building a multilingual puzzle";
+	char String[] = "\"Programming is like building a multilingual puzzle";
 	puts(String);
        	return (0);
 
