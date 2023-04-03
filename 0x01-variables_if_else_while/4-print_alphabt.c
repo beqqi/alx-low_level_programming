278e71b339a4d9128151d4347c63c9e9f9fd4e69
Hulum <mailtoahlam003@gmail.com>
2023-04-02 17:21:07 -0700

    lower alphabet except q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+if (alphabet == 'e' || alphabet == 'q')
+{
+continue;
+}
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
