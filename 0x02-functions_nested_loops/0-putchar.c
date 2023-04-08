3d15417aca8e7432cfb8763990901f49e95277ca
Hulum <mailtoahlam003@gmail.com>
2023-04-07 17:53:41 -0700

    __putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point
  * Return: Always 0 (Success)
  */
 int _putchar(char c)
 {
-write(1, &c, 1);
-write(1, \n, 1);
-return 0
+return (write(1, &c, 1));
 }
-int main(void)
-{
-_putchar(10);
