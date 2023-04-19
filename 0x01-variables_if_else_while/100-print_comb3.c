53c75932945506b4a60d9e7b438cc8eb44c3e8e3
Hulum <mailtoahlam003@gmail.com>
2023-04-19 00:17:34 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 10; i < 100; i++)
+{
+putchar(i);
+putchar(',');
+putchar("\n");
+}
+return (0);
+}
